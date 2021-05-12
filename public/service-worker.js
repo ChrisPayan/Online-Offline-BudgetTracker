bc21805cf664cea0501fbde33e24627ebb5f1f2a
Christian Payan <payanstocks@gmail.com>
2021-05-11 22:05:30 -0700

    Edited cache files to match app

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,9 @@
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/favorites.html',
-    '/topic.html',
-    '/assets/css/style.css',
-    '/dist/app.bundle.js',
-    '/dist/favorites.bundle.js',
-    '/dist/topic.bundle.js',
-    'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
-    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/index.html",
+    "/styles.css",
+    "/index.js",
 ];
 
 const PRECACHE = 'precache-v1';
