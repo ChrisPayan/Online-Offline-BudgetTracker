23d9a9600f1517849d7daf442f0516278a087289
Christian Payan <payanstocks@gmail.com>
2021-05-11 22:15:32 -0700

    Modifed activate when deleting old cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,8 +14,8 @@ self.addEventListener('install', (event) => {
         caches
             .open(PRECACHE)
             .then((cache) => {
-                cache.addAll(FILES_TO_CACHE);
                 console.log("Your files were pre-cached succefully");
+                return cache.addAll(FILES_TO_CACHE);
             })
     );
     self.skipWaiting()
@@ -35,12 +35,13 @@ self.addEventListener('activate', (event) => {
             .then((cachesToDelete) => {
                 return Promise.all(
                     cachesToDelete.map((cacheToDelete) => {
+                        console.log("Removing old cache data.")
                         return caches.delete(cacheToDelete);
                     })
                 );
             })
-            .then(() => self.clients.claim())
     );
+    self.clients.claim()
 });
 
 self.addEventListener('fetch', (event) => {
