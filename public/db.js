368117e62a64f78bb49f61ec9bdca20d4047fdfa
Christian Payan <payanstocks@gmail.com>
2021-05-11 22:27:26 -0700

    Added file from class activity that should work as a baseline for this homework assignment

---
+++ public/db.js
@@ -0,0 +1,79 @@
+let db;
+// create a new db request for a "BudgetDB" database.
+const request = window.indexedDB.open('BudgetDB', 1);
+
+// TODO: setup database with BudgetStore object store.
+request.onupgradeneeded = function (event) {
+    // create object store called "BudgetStore" and set autoIncrement to true
+    db = event.target.result;
+
+    db.createObjectStore('Budget', {
+        keyPath: 'budgetId', autoIncrement: true,
+    });
+};
+
+request.onsuccess = function (event) {
+    console.log("open BudgetDB success 🔌");
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        console.log("Online! 🌐");
+        checkDatabase();
+    }
+};
+
+request.onerror = function (event) {
+    console.log(`⛔ ${e.target.errorCode}`);
+};
+
+// TODO: checkDatabase is called when the user goes online. This function should
+// get all items in the BudgetStore object store and send a request to the
+// backend to add them to the database. If the request is successful, all items
+// should be removed from the BudgetStore object store.
+function checkDatabase() {
+    // open a transaction on your pending db
+    const transaction = db.transaction(['Budget']);
+    // access your BudgetStore object
+    const budgetStore = transaction.objectStore('Budget');
+    // get all entries in the budgetStore
+    const getAll = budgetStore.getAll()
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((response) => response.json())
+                .then((res) => {
+                    if (res.length > 0) {
+                        // create a new readwrite transaction with the BudgetStore
+                        const transaction = db.transaction(['Budget'], 'readwrite');
+                        // access the BudgetStore object store
+                        const budgetStore = transaction.objectStore('Budget');
+                        // clear all items in BudgetStore
+                        budgetStore.clear();
+                    }
+                });
+        }
+    };
+}
+
+// TODO: saveRecord accepts a record object and saves it in the BudgetStore
+// object store
+function saveRecord(record) {
+    // create a transaction on the pending db with readwrite access
+    const transaction = db.transaction(['Budget'], 'readwrite');
+    // access your pending object store
+    const pendingStore = transaction.objectStore('Budget');
+    // add record to your store with add method.+
+    pendingStore.add(record);
+
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
