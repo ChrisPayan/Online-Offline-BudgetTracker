015a06bda7507acf903b3f1a8e34aa87dee42187
Christian Payan <payanstocks@gmail.com>
2021-05-11 23:06:48 -0700

    bug fixes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,65 +1,76 @@
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 const FILES_TO_CACHE = [
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "/index.html",
+    "/manifest.json",
     "/styles.css",
     "/index.js",
+    "/db.js",
 ];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+// install
+self.addEventListener('install', function (evt) {
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches
-            .open(PRECACHE)
-            .then((cache) => {
-                console.log("Your files were pre-cached succefully");
-                return cache.addAll(FILES_TO_CACHE);
-            })
+    // pre cache all static assets
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
     );
-    self.skipWaiting()
+
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
+    self.skipWaiting();
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-        caches
-            .keys()
-            .then((cacheNames) => {
-                return cacheNames.filter(
-                    (cacheName) => !currentCaches.includes(cacheName)
-                );
-            })
-            .then((cachesToDelete) => {
-                return Promise.all(
-                    cachesToDelete.map((cacheToDelete) => {
-                        console.log("Removing old cache data.")
-                        return caches.delete(cacheToDelete);
-                    })
-                );
-            })
+// activate
+self.addEventListener('activate', function (evt) {
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(
+                keyList.map((key) => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Removing old cache data', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
     );
-    self.clients.claim()
+
+    self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
-        event.respondWith(
-            caches.match(event.request).then((cachedResponse) => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
+// fetch
+self.addEventListener('fetch', function (evt) {
+    if (evt.request.url.includes('/api/')) {
+        evt.respondWith(
+            caches
+                .open(DATA_CACHE_NAME)
+                .then((cache) => {
+                    return fetch(evt.request)
+                        .then((response) => {
+                            // If the response was good, clone it and store it in the cache.
+                            if (response.status === 200) {
+                                cache.put(evt.request.url, response.clone());
+                            }
 
-                return caches.open(RUNTIME).then((cache) => {
-                    return fetch(event.request).then((response) => {
-                        return cache.put(event.request, response.clone()).then(() => {
                             return response;
+                        })
+                        .catch((err) => {
+                            // Network request failed, try to get it from the cache.
+                            return cache.match(evt.request);
                         });
-                    });
-                });
-            })
+                })
+                .catch((err) => console.log(err))
         );
+
+        return;
     }
+
+    evt.respondWith(
+        caches.match(evt.request).then(function (response) {
+            return response || fetch(evt.request);
+        })
+    );
 });
